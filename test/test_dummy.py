d55a7c29a2db1839bd3b2e53a65c4aa98dce5110
Kyle Coble <kwc57@cornell.edu>
2023-01-20 15:08:52 -0500

    Rename project

--- test/test_dummy.py
+++ test/test_dummy.py
@@ -1,7 +1,7 @@
 """Template test module."""
 import pytest
-from amiga_package import __version__
-from amiga_package import ops
+from virtual_joystick import __version__
+from virtual_joystick import ops
 
 # import the internal libs and test
 
